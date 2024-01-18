ed107e6dc3ddd09e93ff26f4693a0ec394e4b3f7
noemi3a <nhenrique@3anet.com.br>
2024-01-17 22:13:06 -0300

    Trocando o texto no evento ao click

--- projeto inicial/script.js
+++ projeto inicial/script.js
@@ -2,15 +2,28 @@ const html = document.querySelector('html');
 const focoBt = document.querySelector('.app__card-button--foco');
 const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
+const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title');
 
 focoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'foco');
+    html.setAttribute('data-context', 'foco');
+    banner.setAttribute('src', '/imagens/foco.png');
+    titulo.innerHTML = `Otimize sua produtividade,<br>
+    <strong class="app__title-strong">mergulhar não importa.</strong>`;
 });
 
 curtoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto');
+    html.setAttribute('data-context', 'descanso-curto');
+    banner.setAttribute('src', '/imagens/descanso-curto.png');
+    titulo.innerHTML = `
+        Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong>
+    `;
 });
 
 longoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo');
+    html.setAttribute('data-context', 'descanso-longo');
+    banner.setAttribute('src', '/imagens/descanso-longo.png');
+    titulo.innerHTML = `
+        Hora de voltar à superfície.<strong class="app__title-strong"> Faça uma pausa longa.</strong>
+    `;
 });
